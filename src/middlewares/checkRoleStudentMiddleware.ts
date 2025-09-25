28f88be53b212082c6213b6732874bdd882f5aa2
Chaichan <chaichanchaisak2549@gmail.com>
2025-09-25 11:42:43 +0700

    check role middleware

---
+++ src/middlewares/checkRoleStudentMiddleware.ts
@@ -0,0 +1,25 @@
+// src/middlewares/checkRoleAdminMiddleware.ts
+import { type Request, type Response, type NextFunction } from "express";
+import { type CustomRequest, type User } from "../libs/types.js";
+import { users } from "../db/db.js";
+
+export const checkRoleStudent = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+
+  const payload = req.user;
+  const token = req.token;
+
+
+  const user = users.find((u: User) => u.username === payload?.username);
+  if (!user || user.role !== "STUDENT") {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized user",
+    });
+  }
+
+  next();
+};
